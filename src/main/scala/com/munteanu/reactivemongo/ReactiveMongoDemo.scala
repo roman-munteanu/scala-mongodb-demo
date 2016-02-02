eb2e87a9650db27afa64b5613dd52dcd05845f2f
Roman Munteanu <Roman.Munteanu@endava.com>
2016-02-02 17:58:17 +0200

    reactive mongo: crud operations

--- src/main/scala/com/munteanu/reactivemongo/ReactiveMongoDemo.scala
+++ src/main/scala/com/munteanu/reactivemongo/ReactiveMongoDemo.scala
@@ -1,6 +1,7 @@
 package com.munteanu.reactivemongo
 
 import reactivemongo.api._
+import reactivemongo.api.indexes.{IndexType, Index}
 import reactivemongo.bson.BSONDocument
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.commands.{MultiBulkWriteResult, WriteResult}
@@ -29,7 +30,7 @@ object ReactiveMongoDemo {
     val timeout = 5.seconds
 
     // drop collection
-    collection.drop()
+//    collection.drop()
 
     // insert operation
     val alice = BSONDocument(
@@ -57,8 +58,8 @@ object ReactiveMongoDemo {
       ),
       BSONDocument(
         "firstName" -> "Lisa",
-        "lastName" -> "Hunter",
-        "username" -> "lisa.hunter",
+        "lastName" -> "Gordon",
+        "username" -> "lisa.gordon",
         "password" -> "2b3c",
         "department" -> "Project Management",
         "isActive" -> true
@@ -66,15 +67,70 @@ object ReactiveMongoDemo {
     )
     Await.result(bulkInsertResult, 10.seconds)
 
+    // create index
+    val futureCreateIndex = collection.indexesManager.create(Index(key = Seq(("username", IndexType.Ascending))))
+    Await.result(futureCreateIndex, timeout)
+
+
     // find
     val futureEmployees: Future[List[BSONDocument]] =
       collection.find(BSONDocument()).cursor[BSONDocument]().collect[List]()
 
-    futureEmployees.map(employess =>
-      employess.foreach(doc =>
+    futureEmployees.map(employees =>
+      employees.foreach(doc =>
         println(s"Employee: ${BSONDocument pretty doc}")
       )
     )
+
+    // find one
+    val where = BSONDocument("username" -> "daniel.fowler")
+    val futureEmployee = collection.find(where).one[BSONDocument]
+    Await.result(futureEmployee, timeout)
+    futureEmployee.map(maybeEmployee =>
+      maybeEmployee match {
+        case Some(employee) => println(s"Fetch one: ${BSONDocument pretty employee}")
+        case _ => println("Employee NotFound")
+      }
+    )
+
+    // TODO skip
+    val limit = 2
+    val futureDataSortedLimited = collection.find(BSONDocument())
+                                    .sort(BSONDocument("firstName" -> -1))
+                                    .cursor[BSONDocument]()
+                                    .collect[List](limit)
+    Await.result(futureDataSortedLimited, timeout)
+    futureDataSortedLimited.map(employees =>
+      employees.foreach(doc =>
+        println(s"Employee Sorted: ${BSONDocument pretty doc}")
+      )
+    )
+
+
+    // remove
+    val futureRemove = collection.remove(BSONDocument("username" -> "alice.parker"))
+    Await.result(futureRemove, timeout)
+    futureRemove.map(lastError => println("Remove operation succeeded: " + lastError.ok))
+
+
+    // multiple update
+    val futureMultipleUpdate =
+      collection.update(selector = BSONDocument("isActive" -> true), update = BSONDocument("$set" -> BSONDocument("department" -> "Testing")), multi = true)
+    Await.result(futureMultipleUpdate, timeout)
+    futureMultipleUpdate.map(lastError => println("Multiple update operation succeeded: " + lastError.ok))
+
+
+    // update or insert
+    val update = BSONDocument("$set" ->
+      BSONDocument(
+        "username" -> "helen.smith",
+        "department" -> "HR",
+        "isActive" -> true
+      )
+    )
+    val futureUpsert = collection.update(BSONDocument("username" -> "helen.smith"), update, upsert = true)
+    Await.result(futureUpsert, timeout)
+    futureUpsert.map(lastError => println("Upsert operation succeeded: " + lastError.ok))
 
     ()
   }
